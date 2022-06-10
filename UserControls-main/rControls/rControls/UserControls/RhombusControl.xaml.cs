198293e801722b8739a6970c5bb95aaf1a3b1017
Leva1993 <97660642+Leva1993@users.noreply.github.com>
2022-06-10 14:52:34 +0300

    Add files via upload

---
+++ UserControls-main/rControls/rControls/UserControls/RhombusControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControls
+{
+    /// <summary>
+    /// Логика взаимодействия для RhombusControl.xaml
+    /// </summary>
+    public partial class RhombusControl : UserControl
+    {
+        public string Header { get; set; }
+        public string Description { get; set; }
+        public ImageSource ColoredSource { get; set; }
+        public ImageSource BlackWhiteSource =>
+            (ColoredSource == null) ? null : new FormatConvertedBitmap((BitmapSource)ColoredSource, PixelFormats.Gray8, null, 0);
+
+        public RhombusControl()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void BtnAction_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Description, Header, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
